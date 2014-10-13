d577f2ce3a5198aa86312eeef6e7f8a86120b4d8
Katie Hoffman <ktahoffman@gmail.com>
2014-10-13 13:09:12 -0400

    add test for #run saving files

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -1,25 +1,23 @@
 describe "Analyzer" do
   
-  let(:short_analyzer)  { Analyzer.new("spec/support/short.txt",  "spec/results/short_result.txt",  2, 10, 100000)}
-  let(:medium_analyzer) { Analyzer.new("spec/support/medium.txt", "spec/results/medium_result.txt", 3, 10, 100000)}
-  let(:long_analyzer)   { Analyzer.new("spec/support/long.txt",   "spec/results/long_result.txt",  4, 10, 100000)}
-
+  let(:analyzer)  { Analyzer.new("spec/support/short.txt",  "spec/results/short_result.txt",  2, 10, 100000) }
+  let(:run)       { Analyzer.run("spec/support/short.txt",  "spec/results/short_result.txt",  2) }
+  let(:results)   { {"short" => "Chvrches,Valerie June", "medium" => "Miranda Lambert,The Flaming Lips", "long" => "Chvrches,St. Vincent"} }
+  
   describe "#initialize" do
     it "is initiated with input path, output path, threshold, k, and m arguments" do
-      [short_analyzer, medium_analyzer, long_analyzer].each do |analyzer|
-        expect { analyzer }.to_not raise_error
-      end
+      expect { analyzer }.to_not raise_error
     end
     it "initiates a new instance of the CountMinSketch class called 'pair_counts'" do
-      expect(short_analyzer.pair_counts.class).to eq(CountMinSketch)
+      expect(analyzer.pair_counts.class).to eq(CountMinSketch)
     end
     it "loads the input file as an array and sets it equal to 'data'" do
-      expect(short_analyzer.data.class).to eq(Array)
-      expect(short_analyzer.data.length).to eq(10)
-      short_analyzer.data.each { |bands| expect(bands.length).to eq(3) }
+      expect(analyzer.data.class).to eq(Array)
+      expect(analyzer.data.length).to eq(10)
+      analyzer.data.each { |bands| expect(bands.length).to eq(3) }
     end
     it "initiates two new sets, 'results' and 'verified_results'" do
-      [short_analyzer.results.class, short_analyzer.verified_results.class].each do |class_name|
+      [analyzer.results.class, analyzer.verified_results.class].each do |class_name|
         expect(class_name).to eq(Set)
       end
     end
@@ -27,10 +25,23 @@ describe "Analyzer" do
 
   describe "#run" do
     it "is called with input path, output path, and threshold" do
-      expect {  Analyzer.run("spec/support/long.txt", "spec/results/long_result.txt", 4) }.to_not raise_error
+      expect { run }.to_not raise_error
     end
+
     it "accepts optional arguments of k and m" do
-      expect {  Analyzer.run("spec/support/long.txt", "spec/results/long_result.txt", 4, 15, 10000) }.to_not raise_error
+      expect { Analyzer.run("spec/support/long.txt", "spec/results/long_result.txt", 4, 15, 10000) }.to_not raise_error
+    end
+
+    it "saves the pair that is repeated at least 'threshold' times in the input to the output" do
+      threshold = 2
+      lengths = ["short", "medium", "long"]
+      lengths.each do |length|
+        input, output = get_paths(length)
+        Analyzer.run(input, output, threshold)
+        result = File.read(output)
+        expect(result).to match(/#{results[length]}/)
+        threshold += 1
+      end
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 require_relative '../config/environment'
 
 RSpec.configure do |config|
+  config.before(:all) do
+    FileUtils.rm_rf(Dir["spec/results/*"]) # remove pre-existing view files
+  end
+  config.after(:all) do
+    FileUtils.rm_rf(Dir["spec/results/*"]) # remove created view files
+  end
   config.order = 'default'
+end
+
+def get_paths(length)
+  ["spec/support/#{length}.txt", "spec/results/#{length}_result.txt"]
 end
